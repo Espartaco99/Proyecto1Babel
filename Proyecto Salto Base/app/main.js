e378dce813b6ed5334082304769b585b20d99238
Diego Garzon Molina <g.molina.diego@gmail.com>
2018-02-27 12:50:14 +0100

    inicio css movil

---
+++ Proyecto Salto Base/app/main.js
@@ -0,0 +1,34 @@
+export class Main {
+    constructor(){
+        console.log('Main loaded')
+        this.vista = {
+            aBtnsMenu: document.querySelectorAll('a'),
+            eArticle : document.querySelector('article'),
+            body : document.querySelector('body'),
+            eMain : document.querySelector('main'),
+            aImports: document.querySelectorAll('link[rel="import"]'),
+            oImports: {}
+        }
+        this.vista.aBtnsMenu.forEach(element => {
+            element.addEventListener('click',this.menuItems.bind(this),false)
+        });
+      
+        this.vista.aImports.forEach(elem=>{
+            this.vista.oImports[elem.title]=elem.import
+        })
+        this._cargarTemplate('home')
+    }
+    menuItems(oEv){
+      
+        this._cargarTemplate(oEv.target.id)
+        oEv.preventDefault()
+          }
+    _cargarTemplate(id){
+        console.log(this.vista.oImports)
+        console.log(id)
+        const IMPORT = this.vista.oImports[id]
+        console.log(IMPORT)
+        const ELEM = IMPORT.querySelector(`#${id}`)
+        this.vista.eMain.innerHTML = ELEM.innerHTML
+    }
+}
